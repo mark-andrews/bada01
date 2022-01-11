6cce6ae6a06f919513af89f027ebe955c0711740
Mark Andrews <mjandrews.org@gmail.com>
2022-01-11 19:33:31 +0000

    Update script day1 and recordings list

--- recordings.md
+++ recordings.md
@@ -4,11 +4,11 @@ These are password protected, and you will have been given this password in the 
 # Day 1
 
 * [Session 1](https://vimeo.com/664403956)
-* [Session 2](https://vimeo.com/664487047) In the first 30 minutes or so, there were audio interuptions. This was due to a usb cable. After the cable was changed, the issues did not occur again.
+* [Session 2](https://vimeo.com/664487047) In the first 30 minutes or so, there were audio interruptions. This was due to a usb cable. After the cable was changed, the issues did not occur again.
 
 # Day 2
 
-* Session 1
+* [Session 1](https://vimeo.com/664932064)
 * Session 2
 
 # Day 3
--- script/script_day1.R
+++ script/script_day1.R
@@ -16,3 +16,46 @@ bernoulli_posterior_plot(n = 250, m = 139, alpha = 2, beta = 2)
 beta_plot(139 + 2, 250 - 139 + 2)
 bernoulli_posterior_plot(n = 250, m = 139, alpha = 2, beta = 2, show_hpd = T)
 bernoulli_posterior_summary(n = 250, m = 139, alpha = 2, beta = 2)
+
+
+bernoulli_posterior_summary(n = 250, m = 139, alpha = 2, beta = 2)
+bernoulli_posterior_plot(n = 250, m = 130, alpha = 2, beta = 2, show_hpd = T)
+
+n <- 250
+m <- 139
+alpha <- 2
+beta <- 2
+get_beta_hpd(m + alpha, n - m + beta)
+
+n <- 25
+m <- 14
+alpha <- 2
+beta <- 2
+beta_plot(alpha, beta)
+bernoulli_posterior_plot(n = n, m = m, alpha = alpha, beta = beta, show_hpd = T)
+bernoulli_posterior_summary(n = n, m = m, alpha = alpha, beta = beta)
+
+# now, change the prior
+n <- 25
+m <- 14
+alpha <- 5
+beta <- 5
+beta_plot(alpha, beta)
+bernoulli_posterior_plot(n = n, m = m, alpha = alpha, beta = beta, show_hpd = T)
+bernoulli_posterior_summary(n = n, m = m, alpha = alpha, beta = beta)
+
+# now, change the prior again
+n <- 25
+m <- 14
+alpha <- 10
+beta <- 10
+beta_plot(alpha, beta)
+bernoulli_posterior_plot(n = n, m = m, alpha = alpha, beta = beta, show_hpd = T)
+bernoulli_posterior_summary(n = n, m = m, alpha = alpha, beta = beta)
+
+# now return to n = 250, m = 139
+n <- 250
+m <- 139
+alpha <- 1
+beta <- 1
+bernoulli_posterior_summary(n = n, m = m, alpha = alpha, beta = beta)
